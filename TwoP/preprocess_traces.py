0b093f03860516e796255d32c3a32554391e44fe
Maria Cozan <maria.cozan@gmail.com>
2023-02-10 17:37:38 +0000

    Added and improved comments to correct_neuropil
    Note there are a few comments left which mark a question (to be discussed)

--- TwoP/preprocess_traces.py
+++ TwoP/preprocess_traces.py
@@ -46,15 +46,19 @@ def correct_neuropil(
     N : np.ndarray [t x nROIs]
         Neuropil traces of ROIs.
     numN : int, optional
-        Number of bins used to partition the distribution of neuropil values. Each bin will be associated with
-        a mean neuropil value and a mean signal value. The default is 20.
+        Number of bins used to partition the distribution of neuropil values. 
+        Each bin will be associated with a mean neuropil value and a mean 
+        signal value. The default is 20.
     minNp : int, optional
-        Minimum values of neuropil considered, expressed in percentile. 0 < minNp < 100. The default is 10.
+        Minimum values of neuropil considered, expressed in percentile.
+        0 < minNp < 100. The default is 10.
     maxNp : int, optional
-        Maximum values of neuropil considered, expressed in percentile. 0 < maxNp < 100, minNp < maxNp. The
+        Maximum values of neuropil considered, expressed in percentile.
+        0 < maxNp < 100, minNp < maxNp. The
         default is 90.
     prctl_F : int, optional
-        Percentile of the measured signal that will be matched to neuropil. The default is 5.
+        Percentile of the measured signal that will be matched to neuropil. 
+        The default is 5.
     verbose : boolean, optional
         Feedback on fitting. The default is True.
 
@@ -63,13 +67,17 @@ def correct_neuropil(
     signal : np.ndarray [t x nROIs]
         Neuropil corrected calcium traces.
     regPars : np.ndarray [2 x nROIs], each row: [intercept, slope]
-        Intercept and slope of linear fits of neuropil (N) to measured calcium traces (F)
+        Intercept and slope of linear fits of neuropil (N) to measured calcium 
+        traces (F)
     F_binValues : np.array [numN, nROIs]
-        Low percentile (prctl_F) values for each calcium trace bin. These values were used for linear regression.
+        Low percentile (prctl_F) values for each calcium trace bin. These
+        values were used for linear regression.
     N_binValues : np.array [numN, nROIs]
-        Values for each neuropil bin. These values were used for linear regression.
+        Values for each neuropil bin. These values were used for linear 
+        regression.
 
-    Based on Matlab function estimateNeuropil (in +preproc) written by Mario Dipoppa and Sylvia Schroeder
+    Based on Matlab function estimateNeuropil (in +preproc) written by Mario 
+    Dipoppa and Sylvia Schroeder
     """
 
     [nt, nROIs] = F.shape
@@ -78,56 +86,64 @@ def correct_neuropil(
     regPars = np.ones((2, nROIs)) * np.nan
     signal = np.ones((nt, nROIs)) * np.nan
 
-    # TODO: set verbose options
-
+    
+    # Computes the F0 traces for Calcium traces and Neuropil traces respectively.
+    # Refer to the function for further details on how this is done.
     F0 = get_F0(F, fs)
     N0 = get_F0(N, fs)
-    Fc = F - F0
+    # Corrects for slow drift by subtracting F0 from F and N traces.
+    Fc = F - F0 
     Nc = N - N0
+    
 
-    ti = np.nanargmin((F0 - N0) / N0, 0)
+    # Determines where the minimum normalised difference between F0 and N0.
+    ti = np.nanargmin((F0 - N0) / N0, 0) 
 
-    lowActivity = np.nanpercentile(F, 50, 0)
+    lowActivity = np.nanpercentile(F, 50, 0) # Calculates the F at the 50th percentile. 
     for iROI in range(nROIs):
         # TODO: verbose options
-
+        # Adds the value of F0 at which point the difference between F and N baselines is minimal.
+        #TODO: WHY
         Fc[:, iROI] += F0[ti[iROI], iROI]
         Nc[:, iROI] += N0[ti[iROI], iROI]
-
+        # Gets the current F and N trace.
         iN = Nc[:, iROI]
         iF = Fc[:, iROI]
 
-        # get low and high percentile of neuropil trace
+        # Gets low and high percentile of neuropil trace.
         N_prct = np.nanpercentile(iN, np.array([minNp, maxNp]), axis=0)
-        # divide neuropil values into numN groups
+        # Divides neuropil values into numN groups.
         binSize = (N_prct[1] - N_prct[0]) / numN
-        # get neuropil values regularly spaced across range between minNp and maxNp
+        # Gets neuropil values regularly spaced across range between minNp and maxNp.
         N_binValues[:, iROI] = N_prct[0] + (np.arange(0, stop=numN)) * binSize
 
-        # discretize values of neuropil between minN and maxN, with numN elements
-        # N_ind contains values: 0...binSize for N values within minNp and maxNp
+        # Discretize values of neuropil between minN and maxN, with numN elements
+        # N_ind contains values: 0...binSize for N values within minNp and maxNp.
+        # Done to determine in which bin each data point belongs to.
         N_ind = np.floor((iN - N_prct[0]) / binSize)
 
-        # for each neuropil bin, find the matching (low percentile) value from F trace
+        # Finds the matching (low percentile) value from F trace for each neuropil bin.
+        # This is to determine values of F that are relatively low as these are unlikely to reflect neural spiking.
         for Ni in range(numN):
             tmp = np.ones_like(iF) * np.nan
             tmp[N_ind == Ni] = iF[N_ind == Ni]
             F_binValues[Ni, iROI] = np.nanpercentile(tmp, prctl_F, 0)
-        # Fit only non-nan values
+        # Fits only non-nan values.
         noNan = np.where(
             ~np.isnan(F_binValues[:, iROI]) & ~np.isnan(N_binValues[:, iROI])
         )[0]
-
+        #TODO: CONTINUE
         # perform linear regression between neuropil and signal bins under constraint that 0<slope<2
         # res, _ = optimize.curve_fit(_linear, N_binValues[noNan, iROI], F_binValues[noNan, iROI],
         #                             p0=(np.nanmean(F_binValues[:, iROI]), 0), bounds=([-np.inf, 0], [np.inf, 2]))
-        # find analytical solution
+        # Finds analytical solution to determine the correction factor
+        #TODO: WHY these equations specifically
         a, b, mse = linearAnalyticalSolution(
             N_binValues[noNan, iROI], F_binValues[noNan, iROI], False
         )
         # regPars[:, iROI] = res
         regPars[:, iROI] = (a, b)
-
+        #TODO: WHY is b (the slope of the linear fit used)
         ## avoid over correction
         # b = min(b, 1)
         corrected_sig = iF - b * iN
@@ -194,7 +210,7 @@ def register_zaxis():
 # TODO
 def get_F0(Fc, fs, prctl_F=8, window_size=60, verbose=True):
     """
-    Determines the lowest fluorescence to use for computing deltaF/F.
+    Determines the baseline fluorescence to use for computing deltaF/F.
    
 
     Parameters
@@ -214,7 +230,7 @@ def get_F0(Fc, fs, prctl_F=8, window_size=60, verbose=True):
     Returns
     -------
     F0 : np.ndarray [t x nROIs]
-        The F0 traces for each ROI.
+        The baseline fluorescence (F0) traces for each ROI.
 
     """
     # Translates the window size from seconds into frames.
@@ -250,8 +266,25 @@ def get_F0(Fc, fs, prctl_F=8, window_size=60, verbose=True):
     return F0
 
 
-# TODO
+# TODO: understand why np.fmax is used
 def get_delta_F_over_F(Fc, F0):
+    """
+    Calculates delta F over F by subtracting F0 from Fc and dividing this by
+    the maximum of the mean F of F0.
+
+    Parameters
+    ----------
+    Fc :np.ndarray [t x nROIs]
+        Calcium traces (measured signal) of ROIs.
+    F0 : np.ndarray [t x nROIs]
+        The baseline fluorescence (F0) traces of ROIs.
+
+    Returns
+    -------
+    np.ndarray [t x nROIs]
+    Normalised fluorescence traces (dF/F) of ROIs.
+
+    """
     return (Fc - F0) / np.fmax(1, np.nanmean(F0, 0))
 
 
