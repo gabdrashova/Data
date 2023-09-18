2148f454cd5a61f4d3376b5b03e17b19dfecbb31
Maria Cozan <maria.cozan@gmail.com>
2023-09-18 11:55:09 +0100

    Update runners.py

--- TwoP/runners.py
+++ TwoP/runners.py
@@ -168,7 +168,6 @@ def _process_s2p_singlePlane(
             # Registers Z stack unless it was already registered and saved.
             if not (os.path.exists(zFileName)):
                 zstack = register_zstack(
-                    zstackPath, spacing=1, piezo=piezo, target_image=refImg, channel = ops['align_by_chan']
                     zstackPath,
                     spacing=1,
                     piezo=piezo,
